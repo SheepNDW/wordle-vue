afa03436d25f670868a33e62022dff885a24d01b
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-03-25 14:57:27 +0800

    refactor: extract constant and variable

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -2,11 +2,13 @@ import { describe, expect, it } from 'vitest';
 
 import { mount } from '@vue/test-utils';
 import WordleBoard from '~/components/WordleBoard.vue';
-import { VICTORY_MESSAGE } from '~/utils/settings';
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from '~/utils/settings';
 
 describe('WordleBoard', () => {
+  const wordOfTheDay = 'TESTS';
+
   it('a victory message appears when user make a guess that matches the word of the day', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } });
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } });
 
     const guessInput = wrapper.find('input[type=text]');
     await guessInput.setValue('TESTS');
@@ -16,13 +18,13 @@ describe('WordleBoard', () => {
   });
 
   it('a defeat message appears if the user makes a guess that is incorrect', async () => {
-    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } });
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay } });
 
     const guessInput = wrapper.find('input[type=text]');
     await guessInput.setValue('WRONG');
     await guessInput.trigger('keydown.enter');
 
-    expect(wrapper.text()).toContain('Better luck next time!');
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
   });
 
   it.todo('no end-of-game message appears if the user has not yet made a guess');
