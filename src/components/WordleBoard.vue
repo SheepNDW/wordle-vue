f5ad77b181719df5c1f4a4ef0a1a74fbfe5a477e
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-03-27 14:46:34 +0800

    style: styling wordleboard

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -14,10 +14,33 @@ const guessSubmitted = ref('');
 </script>
 
 <template>
-  <GuessInput @guess-submitted="(guess) => (guessSubmitted = guess)" />
+  <main class="flex flex-col items-center mt-12">
+    <GuessInput @guess-submitted="(guess) => (guessSubmitted = guess)" />
 
-  <p
-    v-if="guessSubmitted.length > 0"
-    v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
-  />
+    <p
+      v-if="guessSubmitted.length > 0"
+      class="end-of-game-message"
+      v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+    />
+  </main>
 </template>
+
+<style scoped>
+.end-of-game-message {
+  font-size: 3rem;
+  animation: end-of-game-message-animation 700ms forwards;
+  white-space: nowrap;
+  text-align: center;
+}
+
+@keyframes end-of-game-message-animation {
+  0% {
+    opacity: 0;
+    transform: rotateZ(0);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(2rem);
+  }
+}
+</style>
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,6 +1,6 @@
 import englishWords from '~/utils/englishWordsWith5Letters.json';
 
-export const VICTORY_MESSAGE = 'You won!';
+export const VICTORY_MESSAGE = '🎉 Congratulations! 🎉';
 export const DEFEAT_MESSAGE = 'Better luck next time!';
 export const DICTIONARY = new Set<string>(englishWords);
 export const WORD_SIZE = 5;
