33d66ea75383307dd4f9104a0fee3e07ad185d29
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2024-04-08 17:09:29 +0800

    test: write e2e test in which user guesses the word of the day correctly

--- cypress/components/WordleBoard.cy.ts
+++ cypress/components/WordleBoard.cy.ts
@@ -1,10 +1,23 @@
 import WordleBoard from '../../src/components/WordleBoard.vue';
+import { VICTORY_MESSAGE } from '../../src/utils/settings';
 import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 
 describe('<WordleBoard />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-vue
-    cy.mount(WordleBoard);
+  it('Displays a victory message after the user provides a guess with the word of the day', () => {
+    const wordOfTheDay = 'TESTS';
+    cy.viewport(550, 750);
+
+    cy.mount(WordleBoard, { props: { wordOfTheDay } });
+
+    cy.get('[autofocus]').focus();
+
+    cy.focused().type('wrong{enter}');
+    cy.focused().type('guess{enter}');
+
+    cy.focused().type(wordOfTheDay);
+    cy.focused().type('{enter}');
+
+    cy.contains(VICTORY_MESSAGE);
   });
 });
